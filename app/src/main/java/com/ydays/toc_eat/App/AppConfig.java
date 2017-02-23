e3cfada565ef383ba14c8402fe5969f9a52c56ac
Rdagast <clement.brossette@ynov.com>
2017-02-23 14:18:09 +0100

    url

--- app/src/main/java/com/ydays/toc_eat/App/AppConfig.java
+++ app/src/main/java/com/ydays/toc_eat/App/AppConfig.java
@@ -9,7 +9,12 @@ public class AppConfig {
     /*
     URL ABOUT SERVER API
      */
-    public static String url ="http://92.222.89.230:500/";
+    public static String url ="http://localhost:3000/";
     public static String url_login = url+"login";
     public static String url_register = url+"register";
+    public static String url_listing = url+"listing";
+    public static String url_listing_insert = url+"listing/insert";
+    public static String url_chat = url+"chat";
+    public static String url_chat_send = url+"chat/send";
+    public static String url_user = url+"user";
 }
